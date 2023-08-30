858f66a445f8b82ef64d218da51f7ef61f088da5
Jprasanto <joniprasanto@gmail.com>
2023-08-30 17:56:01 +0700

    feat add 2nd website

---
+++ website2/src/lib/supabase.js
@@ -0,0 +1,36 @@
+import { createClient } from '@supabase/supabase-js'
+
+import { SUPABASE_URL } from './config'
+import { useRef } from 'react'
+
+let supabase
+
+export const getSupabase = () => {
+  let ticket
+
+  try {
+    ticket = JSON.parse(localStorage.getItem('yujin:ticket'))
+  } catch {
+    ticket = {}
+  }
+
+  if (ticket && ticket.accessToken) {
+    supabase = createClient(SUPABASE_URL, ticket.accessToken, {
+      auth: {
+        autoRefreshToken: false,
+        persistSession: false,
+      },
+      global: {
+        fetch,
+      },
+    })
+  }
+
+  return supabase
+}
+
+export const useSupabase = () => {
+  const supabase = useRef(getSupabase())
+
+  return supabase.current
+}
